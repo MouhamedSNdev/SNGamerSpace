cb405419de95a7882300fab39eff087c4078436e
MouhamedSNdev <mouhamedsnprsnl@gmail.com>
2025-01-22 08:15:59 -0500

    kk

---
+++ src/components/hero.jsx
@@ -0,0 +1,25 @@
+import pugb from "../assets/images/pugb.webp"
+import codm from "../assets/images/codm.webp"
+import freefire from "../assets/images/freefire.webp"
+import efootball from "../assets/images/efootball.webp"
+
+function Hero() {
+	return (
+		<section>
+			<h1>Bienvenue sur SNGamerSpace </h1>
+			<p>
+				Participez à des tournois, connectez-vous avec d'autres gamers
+				et restez informé des dernières actualités du gaming.
+			</p>
+            <div className="supportGame" >
+                <img src={pugb} alt="Une image de Pubg mobile sengal" loading="lazy" />
+                <img src={codm} alt="Une image de Call of dudy Mobile Sengal" loading="lazy" />
+                <img src={freefire} alt="Une imgae de FreeFire Mobie Senegal" loading="lazy" />
+                <img src={efootball} alt="Une image de EfootBall Mobile Senegal" loading="lazy" />
+            </div>
+            <button>Explorer</button>
+		</section>
+	);
+}
+
+export default Hero;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,15 @@
-import NavBar from "../components/NavBar";
+import Hero from '../components/hero';
+import NavBar from '../components/NavBar';
 
 function Home() {
-    return(
-        <>
-            <NavBar />
-        </>
-    )
+	return (
+		<>
+			<NavBar />
+			<main>
+				<Hero />
+			</main>
+		</>
+	);
 }
 
 export default Home;
